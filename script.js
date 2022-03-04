97000743542715a6e80930eeb4150fb35c7aa900
Shreedhar-sanakar <shridhar.sanakar@gmail.com>
2022-03-04 09:31:40 +0530

    Contact Us

---
+++ script.js
@@ -0,0 +1,37 @@
+let data = [
+    {
+      name: "Joey",
+      age: 3,
+      type: "dog",
+    },
+  
+    {
+      name: "Lizzy",
+      age: 6,
+      type: "dog",
+    },
+  
+    {
+      name: "Red",
+      age: 2,
+      type: "dog",
+    },
+  
+    {
+      name: "Sheru",
+      age: 4,
+      type: "dog",
+    },
+  
+    {
+      name: "Butters",
+      age: 6,
+      type: "dog",
+    },
+  
+    {
+      name: "john",
+      age: 45,
+      type: "human",
+    },
+  ];
